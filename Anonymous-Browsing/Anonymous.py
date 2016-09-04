4547d5e3a17e4e22630631475a1d527bad819aaf
Moses Arocha <carocha24@gmail.com>
2016-09-03 23:25:44 -0500

    Needs To Be Used With The Cookie/Session Retrieval

--- Anonymous-Browsing/Anonymous.py
+++ Anonymous-Browsing/Anonymous.py
@@ -2,31 +2,39 @@
 # written by Moses Arocha
 # Written with the help of TJ O'Connor in his book "Violent Python"
 
-import mechanize, cookielib, random
+
+import mechanize
+import cookielib
+import random
+
 
 class Anonymous(mechanize.Browser):
-	def __init__(self, proxies = [], user_agents = []):
-		mechanize.Browser.__init__(self)
-		self.set_handle_robots(False)
-		self.proxies = proxies
-		self.user_agents = user_agents + ['Microsoft/8.0 ', 'Internet Explorer/11.01', 'ExactSearch', 'Nokia7110/1.0']	# Sets the variables accessed for in the alter_user_agent function
-		self.cookie_jar = cookielib.LWPCookieJar()
-		self.set_cookiejar(self.cookie_jar)
-		self.anonymize()						# Will self anonymize the computer
-	def clear_cookies(self):
-		self.cookie_jar = cookielib.LWPCookieJar()
-		self.set_cookiejar(self.cookie_jar)
+    def __init__(self, proxies = [], user_agents = []):
+	mechanize.Browser.__init__(self)
+	self.set_handle_robots(False)
+	self.proxies = proxies
+	self.user_agents = user_agents + ['Microsoft/8.0 ', 'Internet Explorer/11.01', 'ExactSearch', 'Nokia7110/1.0']	
+	self.cookie_jar = cookielib.LWPCookieJar()
+	self.set_cookiejar(self.cookie_jar)
+        self.anonymize()						
+	
+    def clear_cookies(self):
+	self.cookie_jar = cookielib.LWPCookieJar()
+	self.set_cookiejar(self.cookie_jar)
+	
 # The information about your computer that will be sent, make sure this is not correct!!
-	def alter_user_agent(self):
-		index = random.randrange(0, len(self.user_agents))
-		self.addheaders = [('User-agent', (self.user_agents[index]))]
-	def alter_proxy(self):
-		if self.proxies:
-			index = random.randrange(0, len(self.proxies))
-			self.set_proxies({'http': self.proxies[index]})
-	def anonymize(self, sleep = False):					# This function will be called by the GetDifferentCookies function
-		self.clear_cookies()						# It will then reference and call the clear_cookies function and will run it on the localhost
-		self.alter_user_agent()						# It will then reference the alteruseragent function, and will begin the process of altering the information that will be sent to the website
-		self.alter_proxy()						# this will randomize the proxy information so that different cookies can be receieved
-		if sleep:
-			time.sleep(90)
+    def alter_user_agent(self):
+	index = random.randrange(0, len(self.user_agents))
+	self.addheaders = [('User-agent', (self.user_agents[index]))]
+	
+    def alter_proxy(self):
+	if self.proxies:
+	    index = random.randrange(0, len(self.proxies))
+	    self.set_proxies({'http': self.proxies[index]})
+
+    def anonymize(self, sleep = False):			# This function will be called by the GetDifferentCookies function.
+        self.clear_cookies()				# It will then reference and call the clear_cookies function and will run it on the localhost.
+	self.alter_user_agent()				# It will then reference the alteruseragent function, and will begin the process of altering the information that will be sent to the website.
+	self.alter_proxy()				# This will randomize the proxy information so that different cookies can be receieved.
+	if sleep:
+	    time.sleep(90)
